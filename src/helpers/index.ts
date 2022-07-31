a6589464f916d8dd83d54519f1accb58e7f608c5
vdiorio <vitor.diorio@yahoo.com.br>
2022-07-31 18:01:15 -0300

    fix(linter): Lint issues on index.ts

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,7 @@
 import fuzzy from 'fuzzy';
-import { CommitInfo, ConventionalCommitTypes } from '../interfaces';
+import {CommitInfo, ConventionalCommitTypes} from '../interfaces';
 import types from '../data/types.json';
+import execa from 'execa';
 
 export function setDescriptionIndent(string: string) {
   const DEFAULT_LENGTH = 6;
@@ -13,14 +14,14 @@ export function setDescriptionIndent(string: string) {
 
 export function searchType(_answers: CommitInfo, input = '') {
   return new Promise((resolve) => {
-    const choices = types.map(({ type, description }: ConventionalCommitTypes) => ({
+    const choices = types.map(({type, description}: ConventionalCommitTypes) => ({
       name: `${setDescriptionIndent(type).yellow.bold} - ${description}`,
       value: type,
     }));
 
     setTimeout(() => {
-      resolve(fuzzy.filter(input, choices.map(({ name }: { name: string }) => name))
-        .map((el: { original: string }) => choices.find((c: { name: string }) => c.name === el.original)));
+      resolve(fuzzy.filter(input, choices.map(({name}: { name: string }) => name))
+          .map((el: { original: string }) => choices.find((c: { name: string }) => c.name === el.original)));
     }, Math.random() * 470 + 30);
   });
 }
