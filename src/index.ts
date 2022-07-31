da7535b5080f6e8684cd5eb4c19d2f3a080de53a
vdiorio <vitor.diorio@yahoo.com.br>
2022-07-31 16:09:24 -0300

    feat(promt): Scope promt

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import 'colors';
 import inquirer, { Answers } from "inquirer";
 import inquirerPrompt from 'inquirer-autocomplete-prompt';
 import { searchType } from "./helpers";
@@ -11,6 +11,18 @@ inquirer.prompt([
     name: 'type',
     message: `Choose a commit ${'type'.yellow.bold}:`,
     source: searchType,
+  },
+  {
+    type: 'input',
+    name: 'scope',
+    message: `Type in a ${'(scope)'.blue.bold}`,
+    transformer(input, { type }) {
+      return `${'Preview:'.grey} "${type.yellow}${!input.length ? '' : `(${input})`.blue}${':'.yellow}"`;
+    },
+    validate(input): boolean | string {
+      if (input === '') return true
+      return  /^\S+$/.test(input) ? true : 'Scope must not contain spaces';
+    }
   },
 ]).then((answers: Answers) => {
     console.log(answers);
