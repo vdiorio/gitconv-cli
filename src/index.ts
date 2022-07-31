4c005a3dd0beea262af348901714536b71970c07
vdiorio <vitor.diorio@yahoo.com.br>
2022-07-31 17:38:43 -0300

    fix(linter): Update eslint configuration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'colors';
-import inquirer, { Answers } from "inquirer";
+import inquirer, {Answers} from 'inquirer';
 import inquirerPrompt from 'inquirer-autocomplete-prompt';
-import { searchType } from "./helpers";
+import {searchType} from './helpers';
 
-inquirer.registerPrompt('autocomplete', inquirerPrompt );
+inquirer.registerPrompt('autocomplete', inquirerPrompt);
 inquirer.prompt([
   {
     type: 'autocomplete',
@@ -16,25 +16,25 @@ inquirer.prompt([
     type: 'input',
     name: 'scope',
     message: `Type in a ${'(scope)'.blue.bold}`,
-    transformer(input, { type }) {
+    transformer(input, {type}) {
       return `${'Preview:'.grey} "${type.yellow}${!input.length ? '' : `(${input})`.blue}${':'.yellow}"`;
     },
     validate(input): boolean | string {
-      if (input === '') return true
-      return  /^\S+$/.test(input) ? true : 'Scope must not contain spaces';
-    }
+      if (input === '') return true;
+      return /^\S+$/.test(input) ? true : 'Scope must not contain spaces';
+    },
   },
   {
     type: 'input',
     name: 'title',
     message: `Type in the commit ${'title'.green.bold}`,
-    transformer: (input, { type, scope }) => {
+    transformer: (input, {type, scope}) => {
       const suffix = `[${input.length}/48] | ${type.yellow}${scope.length ? `(${scope})`.blue : ''}${':'.yellow}`;
       return `${suffix} ${input.green}`;
     },
     validate(input) {
       return input.length > 0 ? true : 'Insert a valid title';
-    }
+    },
   },
   {
     type: 'input',
